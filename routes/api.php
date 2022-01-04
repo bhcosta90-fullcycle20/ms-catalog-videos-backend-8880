7739fe4a05e1b597e149ec2d414ca85788c1e862
Bruno Costa <bruno.costa@superlogica.com>
2022-01-04 07:53:24 -0300

    status code

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,6 @@ use App\Http\Controllers\{
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', fn() => response()->json(['success' => true]));
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('genres', GenreController::class);
