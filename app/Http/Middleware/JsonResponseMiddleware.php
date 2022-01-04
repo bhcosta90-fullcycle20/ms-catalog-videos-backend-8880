4a2aca2d967ba4dbebe7fcccc1423b0c145b8a93
Bruno Costa <bruno.costa@superlogica.com>
2022-01-04 07:06:18 -0300

    colocando application json

--- app/Http/Middleware/JsonResponseMiddleware.php
+++ app/Http/Middleware/JsonResponseMiddleware.php
@@ -7,17 +7,11 @@ use Illuminate\Http\Request;
 
 class JsonResponseMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
     public function handle(Request $request, Closure $next)
     {
         $request->headers->add([
             'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
         ]);
         return $next($request);
     }
